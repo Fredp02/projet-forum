123463eb2c97db4c3f017627374f9930cf5780e5
Frédéric <f.poulain@gmx.com>
2023-08-22 17:14:35 +0200

    Pagination terminée. Suite : refactoring du system de pagination

--- Controllers/SearchController.php
+++ Controllers/SearchController.php
@@ -18,15 +18,18 @@ use Models\UsersModel;
 
 class SearchController extends MainController
 {
-    private $searchModel;
-    private $categorysModel;
-    private $usersModel;
+    private const LIMITE = 4;
+
+    private SearchModel $searchModel;
+    private CategorysModel $categorysModel;
+    private UsersModel $usersModel;
 
     public function __construct()
     {
         $this->searchModel = new SearchModel();
         $this->categorysModel = new CategorysModel();
         $this->usersModel = new UsersModel();
+
     }
 
 
@@ -56,60 +59,102 @@ class SearchController extends MainController
             if (empty($key)) {
                 // Le champ "Mots clés" est vide
                 throw new Exception('Vous devez entrer un mot clé pour effectuer une recherche');
-            } else {
-
-                // Récupération des données du formulaire
-                $key = isset($_GET['key']) ? htmlspecialchars($_GET['key']) : '';
-                $title = isset($_GET['title']); //isset($_GET['title']) ? true : false;
-                $author = isset($_GET['author']) ? htmlspecialchars($_GET['author']) : '';
-                $from = isset($_GET['from']) ? htmlspecialchars($_GET['from']) : '';
-                $to = isset($_GET['to']) ? htmlspecialchars($_GET['to']) : '';
-                $select = $_GET['select'] ?? ''; //isset($_GET['select']) ? $_GET['select'] : '';
-                $order = isset($_GET['order']) ? htmlspecialchars($_GET['order']) : '';
-                $sort = isset($_GET['sort']) ? htmlspecialchars($_GET['sort']) : '';
-
-
-                if ($author && !$this->usersModel->getUserByPseudo($author)) {
-                    throw new Exception('Ce membre n\'a pas été trouvé');
-                }
+            }
+//            dd($_SERVER);
+// Récupération des données du formulaire
+            $key = isset($_GET['key']) ? htmlspecialchars($_GET['key']) : '';
+            $title = isset($_GET['title']); //isset($_GET['title']) ? true : false;
+            $author = isset($_GET['author']) ? htmlspecialchars($_GET['author']) : '';
+            $from = isset($_GET['from']) ? htmlspecialchars($_GET['from']) : '';
+            $to = isset($_GET['to']) ? htmlspecialchars($_GET['to']) : '';
+            $select = $_GET['select'] ?? ''; //isset($_GET['select']) ? $_GET['select'] : '';
+            $order = isset($_GET['order']) ? htmlspecialchars($_GET['order']) : '';
+            $sort = isset($_GET['sort']) ? htmlspecialchars($_GET['sort']) : '';
+            $numPage = isset($_GET['numPage']) ? (int)htmlspecialchars($_GET['numPage']) : 1;
+            // Je convertis $numPage en "int" dans tous les cas. Cela garantit que c'est un "int" et permettra ensuite d'utiliser la stricte égalité.
 
 
-                //on nettoie la chaine en supprimant les "mots vides" et en utilisant un stemmer
-                $string = Toolbox::cleanSearch($key);
-
-                $initQuery = new QueryBuilder($title, $author, $from, $to, $select, $order, $sort, false, null, null);
-                $limite = 3;
-                $nombreResultatTotal = count($this->searchModel->search($initQuery->create(), $string));
-                $nombrePageTotal = ceil($nombreResultatTotal / $limite);
+            if ($author && !$this->usersModel->getUserByPseudo($author)) {
+                throw new Exception('Ce membre n\'a pas été trouvé');
+            }
 
 
-                // Exécution de la requête SQL et affichage des résultats
+            //on nettoie la chaine en supprimant les "mots vides" et en utilisant un stemmer
+            $string = Toolbox::cleanSearch($key);
 
-                $offset = ($numPage - 1) * $limite;
-                $queryPaginated = new QueryBuilder($title, $author, $from, $to, $select, $order, $sort, true, $limite, $offset);
+            //j'initialise la requete sans pagination, ni limite ni offset
+            $initQuery = new QueryBuilder($title, $author, $from, $to, $select, $order, $sort, false, null, null);
+            $result = $this->searchModel->search($initQuery->create(), $string);
+            //si aucun résultats, on entre dans le catch
 
-                $result = $this->searchModel->search($queryPaginated->create(), $string);
-                if (!$result) {
+            //si on entre manuellement un numero de page dans l'url ou s'il n'y a pas de résultat
+            if ($numPage < 1 || !$result) {
+                throw new Exception('Aucun résultat');
+            }
+
+            $nombreResultatTotal = count($result);
+            $paginator = false;
+            /**
+             * $nombreResultatTotal
+             * $numPage
+             * LIMTE
+             */
+            $NBR_LINKS_PAGINATOR = 3;
+            if ($nombreResultatTotal > self::LIMITE) {
+
+                //Pagintation true:
+                $paginator = true;
+
+
+                //Je détermine le nombre de résultats visible dans la page
+                //ainsi que le nombre total de pages pour gérer l'affichage du chevron droit :
+                $nombrePageTotal = (int)ceil($nombreResultatTotal / self::LIMITE);
+                // Attention ici, je convertis $nombrePageTotal en "int" car le "ceil" garde la variable en "float". Cela permettra ensuite d'utiliser la stricte égalité.
+                if ($numPage > $nombrePageTotal) {
                     throw new Exception('Aucun résultat');
                 }
+                //on calcule l'offset en fonction du numéro de la page et de la limite
+                $offset = ($numPage - 1) * self::LIMITE;
+                //et on initialise la requete finale avec la pagination = "true" + limite et offset
+                $queryPaginated = new QueryBuilder($title, $author, $from, $to, $select, $order, $sort, true, self::LIMITE, $offset);
+
+                //Et on lance le model pour récupérer les résultats
+                $result = $this->searchModel->search($queryPaginated->create(), $string);
 
 
-                $data_page = [
-                    "pageDescription" => "Résultat de la recherche",
-                    "pageTitle" => "Résultat rcherche",
-                    "view" => "../Views/search/viewDisplaySearch.php",
-                    "template" => "../Views/common/template.php",
-                    "css" => "./style/displaySearchStyle.css",
-                    'result' => $result,
-                    'nombrePageTotal' => $nombrePageTotal,
-                    'numPage' => $numPage,
-                    'limit' => $limite,
-                    'key' => $key
-                    // "script" => "./js/validFormRegister.js",
-                ];
-                $this->render($data_page);
+                //On initialise l'uri sans la valeur du numéro de la page. Cela nous permettra dans la vue,
+                // de concaténer l'uri avec un numéro de page spécifique à la pagination.
+                $baseUri = preg_replace('/&numPage=\d+/', '', $_SERVER['REQUEST_URI']) . '&numPage=';
+
+                $targetPage = -1;
+                if ($numPage === 1) {//Si on est sur la page 1, $targetPage = 0
+                    $targetPage = 0;
+                }
+                if ($nombrePageTotal < $NBR_LINKS_PAGINATOR) {
+                    $NBR_LINKS_PAGINATOR = $nombrePageTotal;
+                }elseif ($numPage === $nombrePageTotal){ //si on est sur la dernière page
+                    $targetPage = -2;
+                }
 
             }
+
+
+            $data_page = [
+                "pageDescription" => "Résultat de la recherche",
+                "pageTitle" => "Résultat rcherche",
+                "view" => "../Views/search/viewDisplaySearch.php",
+                "template" => "../Views/common/template.php",
+                "css" => "./style/displaySearchStyle.css",
+                'result' => $result,
+                'numPage' => $numPage,
+                'paginator' => $paginator,
+                'key' => $key,
+                'baseUri' => $baseUri ?? '',
+                'targetPage' => $targetPage ?? 0,
+                'nombrePageTotal' => $nombrePageTotal ?? 1,
+                'nbrLinksPaginator' => $NBR_LINKS_PAGINATOR
+            ];
+            $this->render($data_page);
         } catch (Exception $e) {
             Toolbox::ajouterMessageAlerte($e->getMessage(), 'rouge');
             header('Location:index.php?controller=search');
@@ -117,22 +162,22 @@ class SearchController extends MainController
         }
 
 
-        // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
-        //     if (!empty($_POST['key'])) {
+//     if (!empty($_POST['key'])) {
 
-        //         //on nettoie la chaine en supprimant les "mots vides" et en utilisant un stemmer
-        //         $string = Toolbox::cleanSearch(htmlspecialchars($_POST['key']));
+//         //on nettoie la chaine en supprimant les "mots vides" et en utilisant un stemmer
+//         $string = Toolbox::cleanSearch(htmlspecialchars($_POST['key']));
 
-        //         $result = $this->searchModel->defaultSearch($string);
-        //         dd($result);
-        //     }
-        // } else {
-        //     Toolbox::ajouterMessageAlerte("Session expirée, veuillez recommencer", 'rouge');
-        //     unset($_SESSION['profil']);
-        //     unset($_SESSION['tokenCSRF']);
-        // }
+//         $result = $this->searchModel->defaultSearch($string);
+//         dd($result);
+//     }
+// } else {
+//     Toolbox::ajouterMessageAlerte("Session expirée, veuillez recommencer", 'rouge');
+//     unset($_SESSION['profil']);
+//     unset($_SESSION['tokenCSRF']);
+// }
         $categorysList = $this->categorysModel->getCategorysOrderByParent();
 
         $data_page = [
--- Views/search/viewDisplaySearch.php
+++ Views/search/viewDisplaySearch.php
@@ -2,35 +2,78 @@
     <div>
 
     </div>
-<!--    <div class="filAriane">-->
-<!--        <a href="index.php">Accueil</a> <i class="fa-solid fa-caret-right"></i>-->
-<!--        <a href="?controller=category&action=index&parentCatID=">wdtgdthgd</a>-->
-<!--        <i class="fa-solid fa-caret-right"></i>-->
-<!--        <a href="?controller=topics&action=list&catID=">srthrth</a>-->
-<!--        <i class="fa-solid fa-caret-right"></i>-->
-<!--    </div>-->
+    <!--    <div class="filAriane">-->
+    <!--        <a href="index.php">Accueil</a> <i class="fa-solid fa-caret-right"></i>-->
+    <!--        <a href="?controller=category&action=index&parentCatID=">wdtgdthgd</a>-->
+    <!--        <i class="fa-solid fa-caret-right"></i>-->
+    <!--        <a href="?controller=topics&action=list&catID=">srthrth</a>-->
+    <!--        <i class="fa-solid fa-caret-right"></i>-->
+    <!--    </div>-->
     <div>
-        <h1>Résultat de recherche pour : '<?=$key;?>'</h1>
+        <h1>Résultat de recherche pour : '<?= $key; ?>'</h1>
     </div>
     <div>
-        <?php foreach ($result as $key) :?>
+        <?php foreach ($result as $key) : ?>
             <div class="blocResultat">
                 <div class="blocResultatHeader">
-                    <a href="?controller=topics&action=thread&threadID=<?=$key->topicID;?>#<?=$key->messageID;?>"><h2><?=$key->topicTitle;?></h2></a>
-                    <h4><?=$key->categoryName;?></h4>
+                    <a href="?controller=topics&action=thread&threadID=<?= $key->topicID; ?>#<?= $key->messageID; ?>">
+                        <h2><?= $key->topicTitle; ?></h2></a>
+                    <h4><?= $key->categoryName; ?></h4>
                 </div>
                 <div class="blocResultatBody">
-                    <p><?=$key->messageText;?></p>
+                    <p><?= $key->messageText; ?></p>
                 </div>
                 <div class="blocResultatFooter">
-                    <div class="totalMessage"><span></span><span><?=$key->totalMessages;?></span></div>
-                    <div class="nbrVues"><span></span><span><?=$key->views;?></span></div>
-                    <div class="author"><span>Par : <?=$key->pseudo;?></span></div>
-                    <div class="dateMessage"><span>le <?= \Controllers\Services\Toolbox::convertDate($key->messageDate);?> </span></div>
+                    <div class="totalMessage"><span></span><span><?= $key->totalMessages; ?></span></div>
+                    <div class="nbrVues"><span></span><span><?= $key->views; ?></span></div>
+                    <div class="author"><span>Par : <?= $key->pseudo; ?></span></div>
+                    <div class="dateMessage">
+                        <span>le <?= \Controllers\Services\Toolbox::convertDate($key->messageDate); ?> </span></div>
                 </div>
             </div>
-        <?php endforeach;?>
+        <?php endforeach; ?>
 
     </div>
+
+
+    <?php if ($paginator) : ?>
+        <div class="paginator">
+            <?php if ($numPage > 1) : ?>
+                <div>
+                    <a href="<?= $baseUri . $numPage - 1 ?>">
+                        <button><i class="fa-solid fa-chevron-left"></i></button>
+                    </a>
+                </div>
+            <?php endif; ?>
+            <?php
+//            dump($numPage);
+//            dump($targetPage);
+            ?>
+            <div>
+                <?php for ($i = 0; $i < $nbrLinksPaginator; $i++): ?>
+                    <!--                    Premiere version : -->
+                    <!--                --><?php //if ($numPage == $nombrePageTotal): ?>
+                    <!--                <a href="--><?php //=$baseUri.$numPage+($i-2) ?><!--"><button>--><?php //= $numPage+($i-2)?><!--</button></a>-->
+                    <!--                --><?php //else :?>
+                    <!--                <a href="--><?php //=$baseUri.($numPage > 1 ? $numPage+($i-1):$numPage+$i);?><!--"><button>--><?php //= $numPage > 1 ? $numPage+($i-1):$numPage+$i ?><!--</button></a>-->
+                    <!--                --><?php //endif;?>
+
+                    <!--                Deuxième version :-->
+
+                    <a href="<?= $baseUri . $numPage + $i + $targetPage ?>">
+                        <button class="<?= $numPage + $i + $targetPage === $numPage ? "btnPaginatorActive" : "" ?>"><?= $numPage + $i + $targetPage ?></button>
+                    </a>
+                <?php endfor; ?>
+
+            </div>
+            <?php if ($numPage < $nombrePageTotal) : ?>
+                <div>
+                    <a href="<?= $baseUri . $numPage + 1 ?>">
+                        <button><i class="fa-solid fa-chevron-right"></i></button>
+                    </a>
+                </div>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
 
 </section>
